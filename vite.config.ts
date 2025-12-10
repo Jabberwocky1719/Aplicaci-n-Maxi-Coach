05c21c2dc0d3849918fc0a3182bcc6ab6c511aa2
Marcelo Arcos <marcelo.arcos@maxikash.mx>
2025-12-10 16:36:50 -0600

    Corregida la base URL para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // CRITICAL for GitHub Pages deployment
+  base: "/Aplicaci-n-Maxi-Coach/",
   build: {
     outDir: 'dist', // Output directory for the build
     rollupOptions: {
